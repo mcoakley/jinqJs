e62335757d5b6c7175b82d034404f75f0f993727
Mike Coakley <mcoakley@acmeframework.com>
2016-12-11 23:03:02 +0000

    Minor updates to formatting

--- tests/spec/regression/issue-18-spec.js
+++ tests/spec/regression/issue-18-spec.js
@@ -1,33 +1,33 @@
 (function() {
   
-describe("issue #18 regression testing", function() {
+  describe("issue #18 regression testing", function() {
   
-  var specData = require("../spec-data.js");
-  var jinqJs = require("../../../index.js");
+    var specData = require("../spec-data.js");
+    var jinqJs = require("../../../index.js");
 
   // Fix for bug 1.6.1 when a collection had a 0, a null was returned
-  it("Check that internal collections are cleared.", function () {
-    var result = new jinqJs()
+    it("Check that internal collections are cleared.", function () {
+      var result = new jinqJs()
       .from(specData.people1)
       .select();
 
-    expect(result.length).toEqual(4);
+      expect(result.length).toEqual(4);
 
-    new jinqJs().delete().at();
+      new jinqJs().delete().at();
 
-    result = new jinqJs()
+      result = new jinqJs()
       .from([])
       .select();
 
-    expect(result.length).toEqual(0);
+      expect(result.length).toEqual(0);
 
-    result = new jinqJs()
+      result = new jinqJs()
       .from(specData.people2)
       .select();
 
-    expect(result.length).toEqual(3);
-  });
+      expect(result.length).toEqual(3);
+    });
     
-});
+  });
 
 })();
